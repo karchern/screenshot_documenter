46e308f10d7b761c1ce77c2b788d814e39cd01a2
Nicolai Karcher <karchernic@gmail.com>
2024-06-06 07:55:04 +0200

    Finalizing functioning runner.py that saves screenshot in powerpoint

--- documenter/runner.py
+++ documenter/runner.py
@@ -6,6 +6,9 @@ import tkinter as tk
 from tkinter import simpledialog
 from tkinter.filedialog import asksaveasfile
 
+from pptx import Presentation 
+from pptx.util import Inches
+import io
 
 class Runner:
     def __init__(self):
@@ -20,6 +23,30 @@ class Runner:
         file = asksaveasfile(mode='w', defaultextension=".txt")
         return file
 
+    def save_screenshots_as_powerpoint(self, output_file_path):
+
+        if not output_file_path.endswith('.pptx'):
+            output_file_path = output_file_path.split('.')[:-1]
+            output_file_path += '.pptx'
+        prs = Presentation() 
+        blank_slide_layout = prs.slide_layouts[6] 
+        left = top = Inches(0)        
+        for screenshot in self.screenshots:
+            slide_height = 7.5
+            slide_width = 10            
+            aspect_ratio = screenshot.size[0] / screenshot.size[1]
+            if aspect_ratio > 1.25:
+                slide_height = 10 / aspect_ratio
+            else:
+                slide_width = 7.5 * aspect_ratio
+
+            slide = prs.slides.add_slide(blank_slide_layout)
+            with io.BytesIO() as output:
+                screenshot.save(output, format='PNG')       
+                pic = slide.shapes.add_picture(output, left, top, height=Inches(slide_height), width=Inches(slide_width))
+        prs.save(output_file_path)
+        return 
+
     def run(self):
         while True:
         # Instantiate listener if not already/
@@ -28,6 +55,7 @@ class Runner:
                 self.screenshot_listeners = Listeners()
                 self.screenshot_listeners.add_listener_and_start('mouse_listener_terminator', self.screenshot_listeners.instantiate_terminator_mouse_listener)
                 self.screenshot_listeners.add_listener_and_start('keyboard_listener', self.screenshot_listeners.instantiate_keyboard_listener_screenshot)
+
             
             if isinstance(self.screenshot_listeners.mouse_coordinates, BoxCoordinates) and self.screenshot_listeners:
                 logging.info('Coordinates received, taking screenshot')
@@ -37,8 +65,7 @@ class Runner:
             if not self.breakout_listener.breakout_listener.running:
                 break
         output_file = self.get_output_file()
-        for screenshot in self.screenshots:
-            screenshot.show()
+        self.save_screenshots_as_powerpoint(output_file.name)
 
             
 
